3ea7fff21917a62ee425fb0a344c26c17727e502
pooja kashyap <156314334+poojakashyap-2920@users.noreply.github.com>
2024-06-10 16:01:16 +0530

    Update IAgentCommisionService.cs

--- RepositoryLayer/Interface/IAgentCommisionService.cs
+++ RepositoryLayer/Interface/IAgentCommisionService.cs
@@ -4,25 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace RepositoryLayer.Interface
 {
-<<<<<<< HEAD
-public interface IAgentCommisionService
-    {
-        public Task<decimal> CalculateCommission(int agentId, int policyId, decimal premiumAmount);
-        public  Task<IEnumerable<Commision>> ViewCommissions(int agentId);
-
-        public Task PayCommission(int agentId);
-
-
-=======
     public interface IAgentCommisionService
     {
 
        public Task<decimal> CalculateCommission(int agentId, int policyId, decimal premiumAmount);
         public Task<IEnumerable<Commision>> ViewCommissions(int agentId);
        public Task PayCommission(int agentId);
->>>>>>> c8ef75a48d6d0f2109f56342f5abb787d9323a7f
     }
 }
