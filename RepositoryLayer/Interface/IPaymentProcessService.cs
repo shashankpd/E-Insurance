cd9d5deb38fde0b37d6758a65b9e770f9c737134
shashankpd <95919220+shashankpd@users.noreply.github.com>
2024-06-10 17:52:58 +0530

    Update IPaymentProcessService.cs

--- RepositoryLayer/Interface/IPaymentProcessService.cs
+++ RepositoryLayer/Interface/IPaymentProcessService.cs
@@ -1,8 +1,5 @@
 ﻿using ModelLayer.Entity;
-<<<<<<< HEAD
-=======
 using ModelLayer.RequestDTO;
->>>>>>> c8ef75a48d6d0f2109f56342f5abb787d9323a7f
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,22 +11,10 @@ namespace RepositoryLayer.Interface
     public interface IPaymentProcessService
     {
         public Task<bool> AddPayment(Payment Payment);
-
-<<<<<<< HEAD
-=======
         public Task<IEnumerable<PaymentModel>> GetAllPayments();
-
         public Task<IEnumerable<PaymentModel>> GetPaymentById(int CustomerId);
-
         public Task<IEnumerable<ReceiptDetails>> GetRecieptByPaymementId(int PaymentId);
         public Task<decimal> CalculatePremium(int policyId, int customerAge, decimal coverageAmount, string policyType, string paymentFrequency,int TermYears);
         public Task FinalizePurchase();
-
-
-
-
-
-
->>>>>>> c8ef75a48d6d0f2109f56342f5abb787d9323a7f
     }
 }
