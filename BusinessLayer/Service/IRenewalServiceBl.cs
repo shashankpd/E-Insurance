1a12402638a22fd8e1fc43eedffdf92f5ca53bde
pooja kashyap <156314334+poojakashyap-2920@users.noreply.github.com>
2024-06-13 13:22:05 +0530

    Update IRenewalServiceBl.cs

--- BusinessLayer/Service/IRenewalServiceBl.cs
+++ BusinessLayer/Service/IRenewalServiceBl.cs
@@ -1,44 +1,11 @@
-﻿/*using BusinessLayer.Interface;
+using BusinessLayer.Interface;
 using ModelLayer.Entity;
-using ModelLayer.RequestDTO;
 using RepositoryLayer.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace BusinessLayer.Service
-{
-   public class IRenewalServiceBl:IRenewalBl
-    {
-        private readonly IRenewal renewal;
-        public IRenewalServiceBl(IRenewal renewal)
-        {
-            this.renewal = renewal;
-        }
-
-        //start
-
-        
-        public CustomerPolicy GetCustomerPolicyById(int customerPolicyId)
-        {
-            return renewal.GetCustomerPolicyById(customerPolicyId);
-        }
-
-        public bool RenewPolicy(int customerPolicyId)
-        {
-            return renewal.RenewPolicy(customerPolicyId);
-        }
-    }
-}
-*/
-
-
-
-using BusinessLayer.Interface;
-using ModelLayer.Entity;
-using RepositoryLayer.Interface;
 
 namespace BusinessLayer.Service
 {
